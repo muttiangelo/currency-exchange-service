43c40193246491739882e5a962bd66b9ad4598a5
angelomutti <amutti.dev@gmail.com>
2023-03-11 15:58:27 -0300

    added CurrencyNotFoundException.java

--- src/main/java/com/mutti/currencyexchangeservice/exception/CurrencyNotFoundException.java
+++ src/main/java/com/mutti/currencyexchangeservice/exception/CurrencyNotFoundException.java
@@ -1,6 +1,5 @@
 package com.mutti.currencyexchangeservice.exception;
 
-import io.micrometer.core.ipc.http.HttpSender;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
